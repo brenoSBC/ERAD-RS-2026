3cb53ad3969d4670a66b6c3fe79915737a219834
brenoSBC <sbcbreno01@gmail.com>
2026-03-23 17:34:09 -0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 set -e
 
 mkdir -p FLINK/flink_throughput_stream \
-         MPI/mpi_throughput \
-         RESIPIPE/resipipe_throughput \
-         bin
+         MPI/mpi_throughput            \
+         RESIPIPE/resipipe_throughput  \
+         bin                           \
+         graphs               
 
 unzip ./FLINK/stream.zip -d ./FLINK/
 unzip ./MPI/execution.zip -d ./MPI/
