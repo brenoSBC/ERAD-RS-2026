3add6306dd551d0e6da5af8dd5b16cd3cfa204d5
brenoSBC <sbcbreno01@gmail.com>
2026-03-24 17:40:04 -0300

    Add files via upload

--- metrics/include/mean.hpp
+++ metrics/include/mean.hpp
@@ -4,21 +4,30 @@
 #include <string>
 #include <vector>
 
-inline double mean(const::std::string name, const::std::string fw, std::vector<double> &app_vector)
+inline std::vector<std::vector<double>> mean(
+    const std::string name,
+    const std::string fw,
+    const std::vector<std::vector<double>> &app_vector)
 {
-    double total = 0;
-    for(double n : app_vector) {
-        total += n;
+    std::vector<std::vector<double>> means;
+
+    std::cout << "\n" << fw << " ########## MEAN PER CONFIG [ " << name << " ] ##########" << std::endl;
+
+    for (size_t i = 0; i < app_vector.size(); ++i)
+    {
+        double total = 0;
+
+        for (double n : app_vector[i])
+        {
+            total += n;
+        }
+
+        double m = total / app_vector[i].size();
+
+        means.push_back({m});
+
+        std::cout << "Config " << i + 1 << ": " << m << std::endl;
     }
-    double mean = total / app_vector.size();
 
-    std::cout << "\n" << fw << " ########## MEAN [ " << name << " ] ##########" << std::endl;
-    std::cout << mean << std::endl;
-
-    return mean;
+    return means;
 }
-
-
-
-
-
